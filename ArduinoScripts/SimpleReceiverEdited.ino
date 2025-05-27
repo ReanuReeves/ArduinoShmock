61d6896b2b65405908c6dba5cb254ba685787cbe
Johann R. Neudeck <johannneudeck@gmx.de>
2025-05-28 00:01:06 +0200

    Update SimpleReceiverEdited.ino

--- ArduinoScripts/SimpleReceiverEdited.ino
+++ ArduinoScripts/SimpleReceiverEdited.ino
@@ -3,6 +3,7 @@
 
 int analog_input = A0; // Analog output of the flame sensor
 int digital_input = 3; // Digital output of the flame sensor
+int led_pin = 13;      // Pin for the LED
 
 void setup() {
     Serial.begin(115200);
@@ -21,6 +22,10 @@ void setup() {
     pinMode(analog_input, INPUT);
     pinMode(digital_input, INPUT);
     Serial.println("KY-026 Flame detection initialized");
+
+    // Initialize LED pin
+    pinMode(led_pin, OUTPUT);
+    digitalWrite(led_pin, LOW); // Ensure LED is off initially
 }
 
 void loop() {
@@ -43,6 +48,9 @@ void loop() {
     int digital_value = digitalRead(digital_input);
     if (digital_value == HIGH) {
         Serial.println("FIRE_DETECTED");
+        digitalWrite(led_pin, HIGH); // Turn on the LED
+    } else {
+        digitalWrite(led_pin, LOW); // Turn off the LED
     }
 
     delay(100); // Small delay to avoid flooding the serial output
